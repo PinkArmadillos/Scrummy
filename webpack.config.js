e06b179b57c36ab98588fc598e6e291a458e5304
Gary Balogh <garybalogh93@gmail.com>
2023-04-07 19:30:40 -0500

    Boilerplate set up

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Development',
+      template: './client/index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      publicPath: '/build',
+      directory: path.resolve(__dirname, 'build'),
+    },
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  },  
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  }
+}
+
