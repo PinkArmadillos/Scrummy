f4f97b1483bc75f23f15accd208bb0657e63c6b8
Gary Balogh <garybalogh93@gmail.com>
2023-04-10 11:07:59 -0500

    Minimal viable product in need of styling

--- client/components/Forms.jsx
+++ client/components/Forms.jsx
@@ -64,33 +64,34 @@ export default function Forms({ fetchCounter, storyList }) {
 
   return (
     <div className="forms">
+      <form id="add-story" onSubmit={addStory}>
+        <h3>New Story</h3>
+        <label htmlFor="story-desc">Description</label>
+        <textarea id="story-desc" name="story-desc" onChange={(e)=>setStoryDesc(e.target.value)}></textarea>
+        <select onChange={(e)=>setStoryColor(e.target.value)}>
+          <option value="" hidden>Choose Color</option>
+          <option value="r213g0b0">Red</option>
+          <option value="r6g167b0">Green</option>
+          <option value="r163g206b241">Uranian Blue</option>
+        </select>
+        <input id="submit-story" type="submit" value="Add Story"/>
+      </form>
+
       <form id="add-task" onSubmit={addTask}>
         <h3>New Task</h3>
-        <label htmlFor="task-desc">Description:</label>
+        <label htmlFor="task-desc">Description</label>
         <textarea id="task-desc" name="task-desc" onChange={(e)=>setTaskDesc(e.target.value)}></textarea>
         <label htmlFor="related-story">Related Story</label>
         <select onChange={(e) => setTaskColor(e.target.value)}>
           {descArr}
         </select>
-        <label htmlFor="task-owner">Task Owner:</label>
+        <label htmlFor="task-owner">Task Owner</label>
         <input id="task-owner" name="task-owner" type="text" onChange={(e)=>setTaskOwner(e.target.value)}></input>
-        <label htmlFor="task-diff">Difficulty:</label>
+        <label htmlFor="task-diff">Difficulty</label>
         <input id="task-diff" name="task-diff" type="number" onChange={(e)=>setTaskDiff(e.target.value)}></input>
         <input type="submit" value="Add Task" id="submit-task"/>
       </form>
 
-      <form id="add-story" onSubmit={addStory}>
-        <h3>New Story</h3>
-        <label htmlFor="story-desc">Description:</label>
-        <textarea id="story-desc" name="story-desc" onChange={(e)=>setStoryDesc(e.target.value)}></textarea>
-        <select onChange={(e)=>setStoryColor(e.target.value)}>
-          <option value="" hidden>Choose Color</option>
-          <option value="red">Red</option>
-          <option value="green">Green</option>
-          <option value="blue">Blue</option>
-        </select>
-        <input id="submit-story" type="submit" value="Add Story"/>
-      </form>
       
     </div>
   )
