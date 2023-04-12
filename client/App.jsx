2269c95da188493b91917c8b1034c18e5899cbbd
Das <dahae0309@hotmail.com>
2023-04-12 15:04:49 -0500

    login/signup frontend

--- client/App.jsx
+++ client/App.jsx
@@ -57,7 +57,7 @@ const App = () => {
     {/* //   <tripContext.Provider value={currentTripValue}> */}
         <RouterProvider router={router} />
     {/* //   </tripContext.Provider> */}
-    // </userContext.Provider>
+     </userContext.Provider>
   )
 }
 
--- client/pages/SignUp/SignUpPage.jsx
+++ client/pages/SignUp/SignUpPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { useNavigate, Form, redirect } from 'react-router-dom';
+import { useNavigate, Form, redirect, useActionData } from 'react-router-dom';
 import { userContext } from '../../context';
 
 const SignUpPage = () => {
@@ -31,41 +31,43 @@ const SignUpPage = () => {
 export const signupAction = async ({ request }) => {
   const { user, setUser } = userContext;
   const submitData = await request.formData();
-  
+
   //need to store username/password to DB, then
-  
-  fetch('/api/signup', {
-    method: 'POST',
-    header: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      username: submitData.get('username'),
-      password: submitData.get('password')
-    })
-  })
-    .then(response => response.json())
-    .then(response => {
-      console.log("info we received from backend", response);
-      //if statement
-      switch (response.status) {
-        case 'valid': {
-          console.log('Signup was successful!');
-          // setUser(response.user); //doing this to make this response.user info accessible from userHomePage
-          return redirect('/UserHomePage');
-        }
-        case 'DuplicateUsername': {
-          return {error: 'This username is taken, please choose another'}
-        }
-        default: {
-          console.log(`The status "${response.status}" sent in the response doesn't match the valid cases.`)
-          alert(`The status "${response.status}" sent in the response doesn't match the valid cases.`);
-          return
-          }
-      }
-    })
-    .catch(err => console.log("error in sending signup info to DB", err));
-   
-   //replace below later
-  // return submission;
+  try {
+    const res = await fetch('/api/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: submitData.get('username'),
+        password: submitData.get('password')
+      })
+    });
+    
+    console.log('in function body after fetch')
+    console.log(res, typeof res);
+    const response = await JSON.parse(res);
+    console.log('after ')
+    console.log("info we received from backend", response);
+
+    if (response.status === 'valid') {
+      console.log('Signup was successful!');
+      await setUser(response.user); //doing this to make this response.user info accessible from userHomePage
+      return redirect('/UserHomePage');
+    }
+
+    if (response.status === 'DuplicateUsername') {
+      return { error: 'This username is taken, please choose another' };
+    }
+
+    return { error: `The status "${response.status}" sent in the response doesn't match the valid cases.` };
+    
+  } catch (err) {
+    console.log('In catch statement')
+    return `You got an error when using signup action: ${err.message}`;
+  }
+  console.log('somehow here')
+  return 'fail'
 }
+
 
 export default SignUpPage;
