11358c80fa63c6fb131d84d82bfe2c16028cacfa
atvanek <99850013+atvanek@users.noreply.github.com>
2023-04-12 17:06:28 -0400

    Refactoring context
    Co-authored-by: ychiu5896 <ychiu5896@users.noreply.github.com>

--- client/components/Scrumboard.jsx
+++ client/components/Scrumboard.jsx
@@ -1,15 +1,11 @@
-import React, { useState, useEffect, createContext, useContext } from 'react';
+import React, { useContext } from 'react';
 import Story from './Story';
 import Task from './Task';
-import {handleonDrop} from './MainContainer';
-import {handleonDragOver} from './MainContainer';
+import { dragContext } from './MainContainer';
 
-
-
-export default function Scrumboard({ storyList, taskList, getData }) {
+export default function Scrumboard({ storyList, taskList }) {
 	// TASKS BY STATUS
-const handleDrop = useContext(handleonDrop);
-const handleDragOver = useContext(handleonDragOver); 
+	const { handleDrop, handleDragOver } = useContext(dragContext);
 
 	const backlogTasks = [];
 	const todoTasks = [];
@@ -17,66 +13,88 @@ const handleDragOver = useContext(handleonDragOver);
 	const toVerifyTasks = [];
 	const doneTasks = [];
 
-  
 	// const hello = taskList[1]
 	// console.log("id?", hello.id)
 	// DIVIDE TASKS BY STATUS
 	for (const el of taskList) {
-    // console.log(el)
+		// console.log(el)
 		if (el.status === 'backlog') {
-			backlogTasks.push(<Task key={el.id} task={el} getData={getData} />);
+			backlogTasks.push(<Task key={el.id} task={el} />);
 		} else if (el.status === 'todo') {
-			todoTasks.push(<Task key={el.id} task={el} getData={getData} />);
+			todoTasks.push(<Task key={el.id} task={el} />);
 		} else if (el.status === 'inProgress') {
-			inProgTasks.push(<Task key={el.id} task={el} getData={getData} />);
+			inProgTasks.push(<Task key={el.id} task={el} />);
 		} else if (el.status === 'toVerify') {
-			toVerifyTasks.push(<Task key={el.id} task={el} getData={getData} />);
+			toVerifyTasks.push(<Task key={el.id} task={el} />);
 		} else if (el.status === 'done') {
-			doneTasks.push(<Task key={el.id} task={el} getData={getData} />);
+			doneTasks.push(<Task key={el.id} task={el} />);
 		}
 	}
 
 	// POPULATE ARRAY OF STORY COMPONENTS
 	const stories = [];
 	for (const el of storyList) {
-		stories.push(<Story  key={el.id} story={el} getData={getData} />);
+		stories.push(<Story key={el.id} story={el} />);
 	}
 
-  // const Arr = [backlogTasks,todoTasks,inProgTasks,toVerifyTasks,doneTasks,stories]
-  // for(let i = 0; i<Arr.length; i++){
-  //   Arr[i].push(<div className='drag' key= 'hello' onDrop= {handleDrop} onDragOver={handleDragOver} ></div>)
-  // }
-
+	// const Arr = [backlogTasks,todoTasks,inProgTasks,toVerifyTasks,doneTasks,stories]
+	// for(let i = 0; i<Arr.length; i++){
+	//   Arr[i].push(<div className='drag' key= 'hello' onDrop= {handleDrop} onDragOver={handleDragOver} ></div>)
+	// }
 
 	// RENDER SCRUMBOARD
 	return (
 		<div className='scrumboard'>
-			<div id='backlog' className='column' onDrop= {handleDrop} onDragOver={handleDragOver} >
+			<div
+				id='backlog'
+				className='column'
+				onDrop={handleDrop}
+				onDragOver={handleDragOver}>
 				<h3>Backlog</h3>
 				<hr />
 				{backlogTasks}
 			</div>
-			<div id='stories' className='column' onDrop= {handleDrop} onDragOver={handleDragOver} >
+			<div
+				id='stories'
+				className='column'
+				onDrop={handleDrop}
+				onDragOver={handleDragOver}>
 				<h3>Stories</h3>
 				<hr />
 				{stories}
 			</div>
-			<div id='todo' className='column' onDrop= {handleDrop} onDragOver={handleDragOver} >
+			<div
+				id='todo'
+				className='column'
+				onDrop={handleDrop}
+				onDragOver={handleDragOver}>
 				<h3>To Do</h3>
 				<hr />
 				{todoTasks}
 			</div>
-			<div id='inProgress' className='column' onDrop= {handleDrop} onDragOver={handleDragOver} >
+			<div
+				id='inProgress'
+				className='column'
+				onDrop={handleDrop}
+				onDragOver={handleDragOver}>
 				<h3>In Progress</h3>
 				<hr />
 				{inProgTasks}
 			</div>
-			<div id='toVerify' className='column' onDrop= {handleDrop} onDragOver={handleDragOver} >
+			<div
+				id='toVerify'
+				className='column'
+				onDrop={handleDrop}
+				onDragOver={handleDragOver}>
 				<h3>To Verify</h3>
 				<hr />
 				{toVerifyTasks}
 			</div>
-			<div id='done' className='column' onDrop= {handleDrop} onDragOver={handleDragOver} >
+			<div
+				id='done'
+				className='column'
+				onDrop={handleDrop}
+				onDragOver={handleDragOver}>
 				<h3>Done</h3>
 				<hr />
 				{doneTasks}
