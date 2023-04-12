6c20c8edc913eb71176bdc084fb1b68ca9717207
Mark Teets <markteets@gmail.com>
2023-04-11 19:08:39 -0600

    React router integrated

---
+++ client/pages/SignUp/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useContext } from 'react';
+import { useNavigate, Form } from 'react-router-dom';
+// import { userContext } from '../context';
+
+const SignUpPage = () => {
+  return (
+    <h1>SignUp Page</h1>
+  )
+};
+
+export default SignUpPage;
---
+++ client/pages/UserHome/UserHomePage.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { userContext } from '../../context';
+import { useNavigate } from 'react-router-dom';
+
+const UserHomePage = () => {
+  return (
+    <h1>User Home Page</h1>
+  )
+};
+
+export default UserHomePage;
