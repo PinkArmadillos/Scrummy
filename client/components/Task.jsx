9c572b04397795e3388c4a01bb50be4943c53e8d
atvanek <99850013+atvanek@users.noreply.github.com>
2023-04-13 10:14:39 -0400

    Centered button in tasks

--- client/components/Task.jsx
+++ client/components/Task.jsx
@@ -73,7 +73,7 @@ export default function Task({ task }) {
 				<span className='task-label'>Difficulty</span>
 				{task.difficulty}
 			</p>
-			<select onChange={(e) => changeStatus(e.target.value)}>
+			{/* <select onChange={(e) => changeStatus(e.target.value)}>
 				<option value='' hidden>
 					Change status
 				</option>
@@ -82,10 +82,13 @@ export default function Task({ task }) {
 				<option value='inProgress'>In Progress</option>
 				<option value='toVerify'>To Verify</option>
 				<option value='done'>Done</option>
-			</select>
-			<button type='button' onClick={() => deleteTask(task.id)}>
-				Delete
-			</button>
+			</select> */}
+			<div
+				style={{ display: 'flex', width: 100 + '%', justifyContent: 'center' }}>
+				<button type='button' onClick={() => deleteTask(task.id)}>
+					Delete
+				</button>
+			</div>
 		</div>
 	);
 }
