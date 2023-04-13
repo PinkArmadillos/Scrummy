ac087f324dcf8d706266eea102294af8fc5b3ff6
ychiu5896 <ychiu5896@gmail.com>
2023-04-13 08:59:58 -0500

    test

--- client/components/Task.jsx
+++ client/components/Task.jsx
@@ -8,25 +8,6 @@ export default function Task({ task, getData }) {
 
 	const handleOnDrag = useContext(onDrag);
 
-	function changeStatus(newStatus) {
-		fetch('/api/task', {
-			method: 'PATCH',
-			body: JSON.stringify({
-				status: newStatus,
-				task_id: task.id,
-			}),
-			headers: {
-				'Content-type': 'application/json',
-			},
-		})
-			.then(() => {
-				getData();
-			})
-			.catch((err) => {
-				console.log({ err: 'Error updating task status' });
-			});
-	}
-
 
 	// MAKE DELTE REQUEST TO DELETE TASK
 	function deleteTask(id) {
@@ -59,16 +40,6 @@ export default function Task({ task, getData }) {
 				<span class='task-label'>Difficulty</span>
 				{task.difficulty}
 			</p>
-			<select onChange={(e) => changeStatus(e.target.value)}>
-				<option value='' hidden>
-					Change status
-				</option>
-				<option value='backlog'>Backlog</option>
-				<option value='todo'>To Do</option>
-				<option value='inProgress'>In Progress</option>
-				<option value='toVerify'>To Verify</option>
-				<option value='done'>Done</option>
-			</select>
 			<button type='button' onClick={() => deleteTask(task.id)}>
 				Delete
 			</button>
